6725c8568fe7447a30e998a4ffb4f5185dc92909
roshan-77 <roshan.khadka.u@gmail.com>
2024-07-01 20:32:53 +1000

    adding files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React Project.</h1>
+      <p>The website has now been hosted.</p>
     </div>
   );
 }
